0f3b088fe9c54488137dd6245ca88938aa194490
Luiz Gustavo <gutfoda@gmail.com>
2024-06-24 22:02:23 -0300

    Inserção de dados na tabela

---
+++ Criação de Inserção de dados/Script.sql
@@ -0,0 +1,55 @@
+-- Inserção de dados:
+insert into cliente (idcliente,nome,cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values(1, 'Manoel', '88828383821', '32323', '2001-01-30', 'M', 'Estudante', 'Brasileira', 'rua Joaquim Nabuco', '23', 'Casa', 'Cidade Nova', 'Porto união', 'SC');
+
+
+insert into cliente (idcliente,nome,cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values(2, 'Geraldo', '12343299929', '56565', '1987-01-04', 'M', 'Engenheiro', 'Brasileira', 'rua das limas ', '200', 'app', 'Centro', 'Poro uniao', 'SC');
+
+
+insert into cliente (idcliente,nome,cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values(3, 'Carlos', '87732323227', '55463', '1967-10-01', 'M', 'Pedreiro', 'Brasileira', 'rua das limas', '300', 'Casa', 'Cto', 'Canoinhas', 'SC');
+
+
+insert into cliente (idcliente,nome,cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values(4, 'Adriana', '12321222122', '98777', '1989-09-10', 'F', 'Jornalista', 'Brasileira', 'rua das limas', '240', 'Casa', 'Sao pedro', 'Porto vitoria', 'PR');
+
+
+insert into cliente (idcliente,nome,cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, bairro, municipio, uf)
+values(5, 'Amanda', '99982838828', '28382', '1991-03-04', 'F', 'Jorn', 'Italiana', 'Av.Central', '100', 'São pedro', 'General Carneiro', 'PR');
+
+
+insert into cliente (idcliente,nome,cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, bairro, municipio, uf)
+values(6, 'Angelo', '99982828181', '12323', '2000-01-01', 'M', 'Professor', 'Brasileiro', 'Av Beira Mar', '300', 'Ctr.', 'São paulo', 'SP');
+
+
+insert into cliente (idcliente,nome,cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values(7, 'Anderson', null, null, null, 'M', 'Prof.', 'Italiano', 'Av.Brasil', '100', 'Apartamento', 'Santa rosa', 'Rio de janeiro', 'RJ');
+
+
+insert into cliente (idcliente,nome,cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values(8, 'Camila', '9998282828', null, '2001-10-10', 'F', 'Professora', 'Norte Americana', 'rua central', '4333', null, 'Centro', 'Uberlandia', 'MG');
+
+insert into cliente (idcliente,nome,cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values(9, 'Cristiano', null, null, null, 'M', 'Estudante', 'Alemã', 'rua do centro', '877', 'Casa', 'centro', 'Porto Alegre', 'RS');
+
+insert into cliente (idcliente,nome,cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values(10, 'Fabricio', '8828282828', '32323', null, 'M', 'Estudante', 'Brasileiro', null, null, null, null, 'Pu', 'SC');
+
+insert into cliente (idcliente,nome,cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values(11, 'Fernanda', null, null, null, 'F', null, 'Brasileira', null, null, null, null, 'Porto unicão', 'SC');
+
+insert into cliente (idcliente,nome,cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values(12, 'Gilmar', '88881818181', '888', '2000-02-10', 'M', 'Estud.', null, 'rua das laranjeiras', '200', null, 'C.Nova', 'Canoinhas', 'SC');
+
+insert into cliente (idcliente,nome,cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values(13, 'Diego', '1010191919', '111939', null, 'M', 'Professor', 'Alemão', 'rua central', '455', 'Casa', 'Cidade N.', 'São paulo', 'SP');
+
+insert into cliente (idcliente,nome,cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values(14, 'Jeferson', null, null, '1983-07-01', 'M', null, 'Brasileiro', null, null, null, null, 'União Vitoria', 'PR');
+
+insert into cliente (idcliente,nome,cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, uf)
+values(15, 'Jessica', null, null, null, 'F', 'Estudante', null, null, null, null, null, 'União vitoria', 'PR');
+
+
+
