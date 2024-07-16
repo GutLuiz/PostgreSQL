fdb507f483574f476c556d34bd29585670681880
Luiz Gustavo <gutfoda@gmail.com>
2024-07-16 11:35:02 -0300

    Parte 2 dos exerc de Funções agregadas

--- Exercicio Funções agregadas/script.sql
+++ Exercicio Funções agregadas/script.sql
@@ -17,6 +17,24 @@ select count (iduf) from municipio where iduf = 1 or iduf = 2;
 --6
 select iduf, count(idmunicipio) from municipio group by iduf
 
+--7
+select count(logradouro) from cliente where logradouro is not null;
+
+--8
+select idmunicipio, count(idcliente) from cliente group by idmunicipio;
+
+--9
+select count (idfornecedor) from fornecedor;
+
+--10
+select idfornecedor, count (idproduto)  from produto group by idfornecedor;
+
+--11
+select avg(valor), idfornecedor from produto group by idfornecedor;
+
+--12 
+select sum(valor) from produto;
+
 
 
 
