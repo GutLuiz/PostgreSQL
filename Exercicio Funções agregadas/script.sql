7164824e8bcceb855ef5806be9443cad314e92bd
Luiz Gustavo <gutfoda@gmail.com>
2024-07-17 11:16:51 -0300

    Parte 3 do exercicio de funções agregadas

--- Exercicio Funções agregadas/script.sql
+++ Exercicio Funções agregadas/script.sql
@@ -35,6 +35,76 @@ select avg(valor), idfornecedor from produto group by idfornecedor;
 --12 
 select sum(valor) from produto;
 
+--13
+select max(nome), max(valor) from produto; 
+
+--14
+select min(nome), min(valor) from produto;
+
+--15
+select avg(valor) from produto;
+
+--16
+select count(idtransportadora) from transportadora;
+
+--17
+select avg(valor) from pedido;
+
+--18
+select idcliente, sum(valor) from pedido group by idcliente;
+
+--19
+select idvendedor, sum(valor) from pedido group by idvendedor;
+
+--20
+select idtransportadora, sum(valor) from pedido group by idtransportadora;
+
+--21
+select data_pedido, sum(valor) from pedido group by data_pedido;
+
+--22
+select idcliente, idtransportadora,idvendedor, sum(valor) from pedido group by idcliente, idtransportadora,idvendedor;
+
+--23
+select sum(valor) from pedido where data_pedido between '2008-04-01' and '2009-12-10' and valor > 200;
+
+--24
+select avg(valor) from pedido where idvendedor = 1;
+
+--25
+select avg(valor) from pedido where idcliente  = 15;
+
+--26
+select count(idpedido) from pedido where idtransportadora = 1;
+
+--27
+select idvendedor, count(idpedido) from pedido group by idvendedor;
+
+--28
+select idcliente, count(idpedido) from pedido group by idcliente;
+
+--29
+select count(idpedido) from pedido where data_pedido between  '2008-04-15' and '2008-04-25';
+
+--30
+select count(idpedido) from pedido where valor > 1000;
+
+
+	
+	
+select * from pedido
+select * from vendedor
+select * from cliente
+select * from transportadora
+
+
+
+
+
+
+
+
+
 
 
 
