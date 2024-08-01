632a91033edbf9d7e3d91a4774c92c63ad303972
Luiz Gustavo <gutfoda@gmail.com>
2024-08-01 10:34:26 -0300

    Exericicio view parte final

--- Exercicio View/script.sql
+++ Exercicio View/script.sql
@@ -33,7 +33,76 @@ left outer join
 left outer join
 	bairro as brr on cln.idbairro = brr.idbairro
 
+-- 2
+create view Municipio_Uf as
+select
+	mnc.nome as Municipio,
+	uf.nome as Unidade_da_Federacao,
+	uf.sigla as Sigla
+from
+	municipio as mnc
+left outer join
+	uf on mnc.iduf = uf.iduf;
 	
+-- 3
+create view Produto_info as
+select
+	prd.nome as Produto,
+	prd.valor as valor,
+	fnd.nome as Fornecedor
+from produto as prd
+left outer join
+	fornecedor as fnd on prd.idfornecedor = fnd.idfornecedor;
+
+-- 4
+create view transportadora_info as
+select
+	trp.nome as transportadora,
+	trp.logradouro as logradouro,
+	trp.numero as numero,
+	uf.nome as Unidade_Da_Federacao,
+	uf.sigla as sigla
+from
+	transportadora as trp
+left outer join
+	municipio as mnc on trp.idmunicipio = mnc.idmunicipio      
+left outer join
+	uf on mnc.iduf = uf.iduf;
+
+-- 5
+create view pedido_info as
+select
+	pdd.data_pedido as data_pedido,
+	pdd.valor as valor,
+	trp.nome as transportadora,
+	cln.nome as cliente,
+	vnd.nome as vendedor
+from
+	pedido as pdd
+left outer join
+	transportadora as trp on pdd.idtransportadora = trp.idtransportadora 
+left outer join
+	cliente as cln on pdd.idcliente = cln.idcliente
+left outer join
+	vendedor as vnd on pdd.idvendedor = vnd.idvendedor;
+
+-- 6
+create view Pedido_produto_info as
+select
+	prd.nome as produto,
+	prdpdd.quantidade as quantidade,
+	prdpdd.valor_unitario as Valor_Unitario
+from
+	pedido_produto as prdpdd
+left outer join
+	produto as prd on prdpdd.idproduto = prd.idproduto
+
+
+
+
+
+
+
 	
 	
 
