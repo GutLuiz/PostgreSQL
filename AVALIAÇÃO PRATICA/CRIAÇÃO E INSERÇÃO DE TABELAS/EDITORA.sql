818414488b8c0eb726db12f72c3d0948bc16d8d0
Luiz Gustavo <gutfoda@gmail.com>
2024-08-12 10:53:27 -0300

    Criação e inserção da tabela editora

--- AVALIAÇÃO PRATICA/CRIAÇÃO E INSERÇÃO DE TABELAS/EDITORA.sql
+++ AVALIAÇÃO PRATICA/CRIAÇÃO E INSERÇÃO DE TABELAS/EDITORA.sql
@@ -5,3 +5,9 @@ create table Editora(
 	constraint pk_edt_idEditora primary key (idEditora),
 	constraint un_edt_nome unique (nome)
 );
+
+insert into Editora (nome) values ('Bookman');
+insert into Editora (nome) values ('Edgard Blusher');
+insert into Editora (nome) values ('Nova terra');
+insert into Editora (nome) values ('Brasport');
+
