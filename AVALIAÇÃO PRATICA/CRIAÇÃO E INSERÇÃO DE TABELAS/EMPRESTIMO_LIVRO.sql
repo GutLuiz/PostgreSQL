d5a3a19271009ad4a6af1f5cc63dfd18ab914f7f
Luiz Gustavo <gutfoda@gmail.com>
2024-08-18 21:36:21 -0300

    Inserindo todos os dados da tabela Emprestimo_livro

--- AVALIAÇÃO PRATICA/CRIAÇÃO E INSERÇÃO DE TABELAS/EMPRESTIMO_LIVRO.sql
+++ AVALIAÇÃO PRATICA/CRIAÇÃO E INSERÇÃO DE TABELAS/EMPRESTIMO_LIVRO.sql
@@ -7,17 +7,17 @@ Create table Emprestimo_Livro (
 	constraint fk_emplvr_idLivro foreign key (idLivro) references Livro(idLivro)
 );
 
-insert into Empretimo_Livro (idEmprestimo, idLivro) values (1,6);
-insert into Empretimo_Livro (idEmprestimo, idLivro) values (3, 4);
-insert into Empretimo_Livro (idEmprestimo, idLivro) values (3, 3);
-insert into Empretimo_Livro (idEmprestimo, idLivro) values (, '');
-insert into Empretimo_Livro (idEmprestimo, idLivro) values ('', '');
-insert into Empretimo_Livro (idEmprestimo, idLivro) values ('', '');
-insert into Empretimo_Livro (idEmprestimo, idLivro) values ('', '');
-insert into Empretimo_Livro (idEmprestimo, idLivro) values ('', '');
-insert into Empretimo_Livro (idEmprestimo, idLivro) values ('', '');
-insert into Empretimo_Livro (idEmprestimo, idLivro) values ('', '');
+insert into Emprestimo_Livro (idEmprestimo, idLivro) values (1,1);
+insert into Emprestimo_Livro (idEmprestimo, idLivro) values (3,4);
+insert into Emprestimo_Livro (idEmprestimo, idLivro) values (3,3);
+insert into Emprestimo_Livro (idEmprestimo, idLivro) values (10,2);
+insert into Emprestimo_Livro (idEmprestimo, idLivro) values (10,7);
+insert into Emprestimo_Livro (idEmprestimo, idLivro) values (6,5);
+insert into Emprestimo_Livro (idEmprestimo, idLivro) values (7,4);
+insert into Emprestimo_Livro (idEmprestimo, idLivro) values (8,6);
+insert into Emprestimo_Livro (idEmprestimo, idLivro) values (8,1);
+insert into Emprestimo_Livro (idEmprestimo, idLivro) values (9,8);
 
-select * from emprestimo
-select * from aluno
-select * from livro
+
+
+
