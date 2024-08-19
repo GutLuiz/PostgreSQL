5de906e17177233c142d175681d293ae9d1914d2
Luiz Gustavo <gutfoda@gmail.com>
2024-08-18 21:30:54 -0300

    Correções

--- AVALIAÇÃO PRATICA/CRIAÇÃO E INSERÇÃO DE TABELAS/EMPRESTIMO.sql
+++ AVALIAÇÃO PRATICA/CRIAÇÃO E INSERÇÃO DE TABELAS/EMPRESTIMO.sql
@@ -12,10 +12,10 @@ Create table Emprestimo (
 
 Insert into Emprestimo (idAluno, Data_Emprestimo, Data_Devolucao, Valor, Devolvido) values ('1', '02/05/2012', '12/05/2012', '10.00', 'S');
 Insert into Emprestimo (idAluno, Data_Emprestimo, Data_Devolucao, Valor, Devolvido) values ('1', '23/04/2012', '03/05/2012', '5.00', 'N');
-Insert into Emprestimo (idAluno, Data_Emprestimo, Data_Devolucao, Valor, Devolvido) values ('7', '10/05/2012', '20/05/2012', '12.00', 'N');
-Insert into Emprestimo (idAluno, Data_Emprestimo, Data_Devolucao, Valor, Devolvido) values ('8', '10/05/2012', '20/05/2012', '8.00', 'S');
-Insert into Emprestimo (idAluno, Data_Emprestimo, Data_Devolucao, Valor, Devolvido) values ('9', '05/05/2012', '15/05/2012', '15.00', 'N');
-Insert into Emprestimo (idAluno, Data_Emprestimo, Data_Devolucao, Valor, Devolvido) values ('9', '07/05/2012', '17/05/2012', '20.00', 'S');
-Insert into Emprestimo (idAluno, Data_Emprestimo, Data_Devolucao, Valor, Devolvido) values ('9', '08/05/2012', '18/05/2012', '5.00', 'S');
+Insert into Emprestimo (idAluno, Data_Emprestimo, Data_Devolucao, Valor, Devolvido) values ('2', '10/05/2012', '20/05/2012', '12.00', 'N');
+Insert into Emprestimo (idAluno, Data_Emprestimo, Data_Devolucao, Valor, Devolvido) values ('3', '10/05/2012', '20/05/2012', '8.00', 'S');
+Insert into Emprestimo (idAluno, Data_Emprestimo, Data_Devolucao, Valor, Devolvido) values ('4', '05/05/2012', '15/05/2012', '15.00', 'N');
+Insert into Emprestimo (idAluno, Data_Emprestimo, Data_Devolucao, Valor, Devolvido) values ('4', '07/05/2012', '17/05/2012', '20.00', 'S');
+Insert into Emprestimo (idAluno, Data_Emprestimo, Data_Devolucao, Valor, Devolvido) values ('4', '08/05/2012', '18/05/2012', '5.00', 'S');
 
 select * from aluno
